f78f13bd5e97377bde1c0c7e99e0f823398765f9
Shubhangi Ambade <shubhangiambade6897@gmail.com>
2020-02-04 13:27:03 +0530

    chnages

--- src/action/action.js
+++ src/action/action.js
@@ -9,7 +9,6 @@
 
 export const ChangeAction = () => {
     return async (dispatch) => {
-
         const data = await fetch('https://jsonplaceholder.typicode.com/users')
         const res2 = await data.json()
         dispatch({
@@ -25,6 +24,5 @@ export const AddWishAction = () => {
         payload: 'code'
     }
 }
-
 
 export default ChangeAction;
